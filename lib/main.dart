52e81fe338266f53e06258f81e190a955519ef64
David Tigre <tigredavid007@gmail.com>
2020-09-18 09:56:19 -0300

    Small Feature - User Message Todo List is Empty

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TODO APP',
-
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-
       home: HomePage(),
     );
   }
@@ -43,25 +40,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var newTaskCtrl = TextEditingController();
 
-  void addItem () {
+  void addItem() {
     var inputText = newTaskCtrl.text;
 
-    if(inputText.isEmpty)
-      return;
+    if (inputText.isEmpty) return;
 
     setState(() {
-      widget.items.add(
-        Item(
-          title: inputText,
-          done: false
-        ));
+      widget.items.add(Item(title: inputText, done: false));
 
       newTaskCtrl.clear();
       save();
     });
   }
 
-  void removeItem (int index) {
+  void removeItem(int index) {
     setState(() {
       widget.items.removeAt(index);
       save();
@@ -72,7 +64,7 @@ class _HomePageState extends State<HomePage> {
     var prefs = await SharedPreferences.getInstance();
     var data = prefs.getString('data');
 
-    if(data != null) {
+    if (data != null) {
       Iterable decoded = jsonDecode(data);
       List<Item> result = decoded.map((el) => Item.fromJson(el)).toList();
 
@@ -112,71 +104,77 @@ class _HomePageState extends State<HomePage> {
         //   Icon(Icons.plus_one)
         // ],
       ),
-      body: ListView.builder(
-        itemCount: widget.items.length,
-        itemBuilder: (BuildContext ctxt, int index) {
-          final item = widget.items[index];
+      body: widget.items.isEmpty
+          ? Center(
+        child: Text(
+          "Sem Tarefas no Momento",
+          textAlign: TextAlign.center,
+        ),
+      )
+          : ListView.builder(
+          itemCount: widget.items.length,
+          itemBuilder: (BuildContext ctxt, int index) {
+            final item = widget.items[index];
 
-          return Dismissible(
-            child: CheckboxListTile(
-              title: Text(item.title),
-              value: item.done,
-              onChanged: (value) {
-                setState(() {
-                  item.done = value;
-                  save();
-                });
+            return Dismissible(
+              child: CheckboxListTile(
+                title: Text(item.title),
+                value: item.done,
+                onChanged: (value) {
+                  setState(() {
+                    item.done = value;
+                    save();
+                  });
+                },
+              ),
+              key: Key(item.title),
+              background: SlideRight(),
+              secondaryBackground: SlideLeft(),
+              confirmDismiss: (direction) async {
+                if (direction == DismissDirection.endToStart) {
+                  final bool res = await showDialog(
+                      context: ctxt,
+                      builder: (BuildContext ctx) {
+                        return AlertDialog(
+                          content: Text("Tem certeza que deseja deletar este item?"),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text(
+                                "Cancel",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              onPressed: () {
+                                Navigator.of(ctx).pop();
+                              },
+                            ),
+                            FlatButton(
+                              child: Text(
+                                "Delete",
+                                style: TextStyle(color: Colors.red),
+                              ),
+                              onPressed: () {
+                                removeItem(index);
+                                Scaffold.of(ctxt).showSnackBar(SnackBar(
+                                  content: new Text("Item eliminado"),
+                                ));
+                                Navigator.of(ctx).pop();
+                              },
+                            )
+                          ],
+                        );
+                      });
+                  return res;
+                } else {
+                  return null;
+                }
               },
-            ),
-            key: Key(item.title),
-            background: SlideRight(),
-            secondaryBackground: SlideLeft(),
-            confirmDismiss: (direction) async {
-              if(direction == DismissDirection.endToStart){
-                final bool res = await showDialog(
-                  context: ctxt,
-                  builder: (BuildContext ctx){
-                    return AlertDialog(
-                      content: Text("Tem certeza que deseja deletar este item?"),
-                      actions: <Widget>[
-                        FlatButton(
-                          child: Text(
-                            "Cancel",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                          onPressed: () {
-                            Navigator.of(ctx).pop();
-                          },
-                        ),
-                        FlatButton(
-                          child: Text(
-                            "Delete",
-                            style: TextStyle(color: Colors.red),
-                          ),
-                          onPressed: () {
-                            removeItem(index);
-                            Scaffold.of(ctxt).showSnackBar(SnackBar(content: new Text("Item eliminado"),));
-                            Navigator.of(ctx).pop();
-                          },
-                        )
-                      ],
-                    );
-                  }
-
-                );
-                return res;
-              } else {
-                return null;
-              }
-            },
 //            onDismissed: (direction) {
 //              //if(direction == DismissDirection.endToStart)
 ////                removeItem(index);
 ////                Scaffold.of(ctxt).showSnackBar(SnackBar(content: new Text("Item eliminado"),));
 //            },
-          );
-        }
-      ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: addItem,
         child: Icon(Icons.add),
